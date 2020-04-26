da98bfef509be14d65c090e7ce175911e9dcd589
Admin <Admin@DESKTOP-G6MJPD6>
2020-04-26 20:16:22 +0300

    вторая фиксация 26-04-20

---
+++ src/javaapplication71/JavaApplication71.java
@@ -0,0 +1,34 @@
+
+package javaapplication71;
+import java.util.Scanner;
+import java.util.function.*;
+public class JavaApplication71 {
+   
+    public static void main(String[] args) {
+         
+    Supplier<User> userFactory = ()->{
+             
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите имя: ");
+            String name = in.nextLine();
+            return new User(name);
+        };
+         
+        User user1 = userFactory.get();
+        User user2 = userFactory.get();
+         
+        System.out.println("Имя user1: " + user1.getName());
+        System.out.println("Имя user2: " + user2.getName());
+    }
+}
+class User{
+     
+    private String name;
+    String getName(){
+        return name;
+    }
+     
+    User(String n){
+        this.name=n;
+    }
+}
